e2946684cf70a29655bee430aaef1ce5fd74cd43
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-10-06 15:06:49 +0200

    preparing for hacs

--- lib.py
+++
@@ -1,5 +0,0 @@
-def make_human_friendly(topic_base: str) -> str:
-    """Make the topic base more human-readable."""
-    parts = topic_base.split("/")[-1]
-    parts = parts.replace("_", " ")
-    return parts.title()
