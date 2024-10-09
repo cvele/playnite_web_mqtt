0510437db599eb5bcd006735ebecd5b89ec71456
Vladimir Cvetic <vladimir@ferdinand.rs>
2024-10-09 09:45:57 +0200

    Add upper limit for max_image_size to prevent extremely large values

--- custom_components/playnite_web_mqtt/config_flow.py
+++ custom_components/playnite_web_mqtt/config_flow.py
@@ -74,9 +74,7 @@ class PlayniteMQTTOptionsFlow(config_entries.OptionsFlow):
                     ),
                 ): str,
                 vol.Optional("max_image_size", default=14500): vol.All(
-                    vol.Coerce(int), vol.Range(min=1000, max=1000000)
-                ),
-                    vol.Coerce(int), vol.Range(min=1000)
+                    vol.Coerce(int), vol.Range(min=1000, max=15000)
                 ),
                 vol.Optional("min_quality", default=60): vol.All(
                     vol.Coerce(int), vol.Range(min=10, max=100)
